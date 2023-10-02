07586e332849b914467b3c81c3bb2bac1550fe06
Vitória Moura <vim.saturnino@gmail.com>
2023-10-02 21:57:24 +0000

    feat: crate user services

---
+++ src/main/java/br/gov/sp/fatec/springproject/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package br.gov.sp.fatec.springproject.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.gov.sp.fatec.springproject.entity.Usuario;
+import br.gov.sp.fatec.springproject.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    public Usuario buscarPorId(Long id) {
+      Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+      if(usuarioOp.isPresent()) {
+        return usuarioOp.get();
+      }
+      throw new IllegalArgumentException("Id inválido!");
+    }
+
+    public Usuario novoUsuario(Usuario usuario) {
+      if(usuario == null ||
+          usuario.getNome() == null ||
+          usuario.getSenha() == null) {
+        throw new IllegalArgumentException("Nome e senha inválidos!");
+      }
+      return usuarioRepo.save(usuario);
+    }
+    public List<Usuario> buscarTodos() {
+      return usuarioRepo.findAll();
+    }
+}
