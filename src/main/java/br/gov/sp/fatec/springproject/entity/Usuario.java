98f2f0a3c6f7dfe2f892243e92ec52a76e1035ff
Vitória Moura <vim.saturnino@gmail.com>
2023-10-03 00:57:35 +0000

    wip: implements relations n-n

--- src/main/java/br/gov/sp/fatec/springproject/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/springproject/entity/Usuario.java
@@ -10,6 +10,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -32,6 +35,12 @@ public class Usuario {
   @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
   @JsonIgnore
   private List<Anotacao> anotacoes;
+
+  @ManyToMany
+  @JoinTable(name = "uau_usuario_autorizacao",
+    joinColumns = {@JoinColumn(name = "usr_id")},
+    inverseJoinColumns = {@JoinColumn(name = "aut_id")})
+  private List<Autorizacao> autorizacoes;
 
   public Usuario() { }
 
--- src/main/java/br/gov/sp/fatec/springproject/service/UsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springproject/service/UsuarioService.java
@@ -1,38 +1,56 @@
 package br.gov.sp.fatec.springproject.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.gov.sp.fatec.springproject.entity.Autorizacao;
 import br.gov.sp.fatec.springproject.entity.Usuario;
+import br.gov.sp.fatec.springproject.exception.UsuarioNaoEncontradoException;
 import br.gov.sp.fatec.springproject.repository.UsuarioRepository;
 import br.gov.sp.fatec.springproject.interfaces.IUsuarioService;
 
 @Service
 public class UsuarioService implements IUsuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepo;
+  @Autowired
+  private UsuarioRepository usuarioRepo;
 
-    public Usuario getById(Long id) {
-      Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
-      if(usuarioOp.isPresent()) {
-        return usuarioOp.get();
-      }
-      throw new IllegalArgumentException("Id inválido!");
-    }
+  @Transactional
+    public Usuario novoUsuarioAutorizacao(String nome,
+        String senha, String nomeAutorizacao) {
+    Usuario usuario = new Usuario(nome, senha);
+    Optional<Autorizacao> autOp = autRepo.findByNome(nomeAutorizacao);
+    Autorizacao autorizacao;
 
-    public Usuario createUser(Usuario usuario) {
-      if(usuario == null ||
-          usuario.getNome() == null ||
-          usuario.getSenha() == null) {
-        throw new IllegalArgumentException("Nome e senha inválidos!");
-      }
-      return usuarioRepo.save(usuario);
+    if(autOp.isEmpty()) {
+      autorizacao = new Autorizacao();
+      autorizacao.setNome(nomeAutorizacao);
+      autRepo.save(autorizacao);
+    } else {
+      autorizacao = autOp.get();
     }
-    public List<Usuario> getAllUsers() {
-      return usuarioRepo.findAll();
+    usuario.setAutorizacoes(new ArrayList<Autorizacao>());
+    usuario.getAutorizacoes().add(autorizacao);
+    
+    return usuarioRepo.save(usuario);
+  }
+
+  public Usuario createUser(Usuario usuario) {
+    if(usuario == null ||
+        usuario.getNome() == null ||
+        usuario.getSenha() == null) {
+      throw new IllegalArgumentException("Nome e senha inválidos!");
     }
+    return usuarioRepo.save(usuario);
+  }
+  public List<Usuario> getAllUsers() {
+    return usuarioRepo.findAll();
+  }
 }
