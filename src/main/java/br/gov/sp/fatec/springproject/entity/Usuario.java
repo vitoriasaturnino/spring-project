01d507f48dfadbc4df8a1d0d34ca1ddd1c12bb46
Vitória Moura <vim.saturnino@gmail.com>
2023-10-03 00:39:37 +0000

    feat: implements relationships between user and annotation tables

--- src/main/java/br/gov/sp/fatec/springproject/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/springproject/entity/Usuario.java
@@ -1,13 +1,17 @@
 package br.gov.sp.fatec.springproject.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "usr_usuario")
@@ -24,6 +28,10 @@ public class Usuario {
   @Column(name = "usr_senha")
   @JsonIgnore
   private String senha;
+
+  @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+  @JsonIgnore
+  private List<Anotacao> anotacoes;
 
   public Usuario() { }
 
